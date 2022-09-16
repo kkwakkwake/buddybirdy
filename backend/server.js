e2e3bf2f22e3fdeda76a788f9717824c6ee14108
kkwakkwake <glowupup@naver.com>
2022-09-17 00:44:07 +0900

    feat:deploy

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,14 @@ app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
 app.use('/api/message', messageRoutes)
 
+console.log(__dirname);
+
 // deploy 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
 
   app.get("*", (req, res) => {
-    res.sendFile(__dirname + '/build/static/index.html')
+    res.sendFile(__dirname + '/build/index.html')
   })
 } else {
   app.get('/', (req, res) => {
